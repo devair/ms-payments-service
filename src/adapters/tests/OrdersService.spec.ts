dcd025586a3d8d9a733e01f9b1836bc28569490c
devair <devairft@gmail.com>
2024-07-03 21:23:09 -0300

    some unit tests

---
+++ src/adapters/tests/OrdersService.spec.ts
@@ -0,0 +1,14 @@
+describe('Orders Service Test', ()=>{
+
+    beforeEach(()=>{
+
+    })
+
+    it('Should be able to update an order', async ()=>{
+
+
+        expect(1).toBe(1)    
+
+    })
+
+})
