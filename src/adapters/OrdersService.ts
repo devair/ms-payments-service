99c70f82186dbca7c5868e077b396eb034618cb7
devair <devairft@gmail.com>
2024-07-10 21:43:02 -0300

    remove console log

--- src/adapters/OrdersService.ts
+++ src/adapters/OrdersService.ts
@@ -7,22 +7,13 @@ class OrdersService implements IOrdersService{
     async updateOrderStatus({ orderId, status }: { orderId: any; status: any; }): Promise<void> {
         
         try {
-            console.log(this.apiStatus)
-
             const response = await fetch(`${this.apiStatus}/orders/${orderId}/status`,{
                 method: 'PATCH',
                 body: JSON.stringify({
                     status: status
                 }),
                 headers: { "Content-Type": "application/json" },
-            })  
-            
-            if(response.ok){
-                console.log(response.body)
-            }
-            else {
-                console.log(response.status)
-            }
+            })                         
 
         } catch (error) {
             console.log(JSON.stringify(error))
