48cd0db876d89b590967d7d602e958baba681df8
devair <devairft@gmail.com>
2024-07-14 18:10:34 -0300

    cucumber report

---
+++ cucumber-report.js
@@ -0,0 +1,21 @@
+var reporter = require('cucumber-html-reporter');
+
+var options = {
+        theme: 'bootstrap',
+        jsonFile: 'reports/cucumber/cucumber-report.json',
+        output: 'reports/cucumber/cucumber-report.html',
+        reportSuiteAsScenarios: true,
+        scenarioTimestamp: true,
+        launchReport: true,
+        metadata: {
+            "App Version":"0.3.2",
+            "Test Environment": "STAGING",
+            "Browser": "Chrome  54.0.2840.98",
+            "Platform": "Windows 10",
+            "Parallel": "Scenarios",
+            "Executed": "Remote"
+        },
+        failedSummaryReport: true,
+    };
+
+    reporter.generate(options);
